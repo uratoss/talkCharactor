b7b265df4813ed2b63a80a3283a0b816d26f70ad
uratoss <adlersshanze@gmail.com>
2019-09-18 18:57:54 +0900

    init

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,15 @@
 var socketio = io();
-var myName='kirasy'
+var myName='user'
 $(function(){
     $('#message_form').submit(function(){
       msg = $('#input_msg').val()
       socketio.emit('message', msg);
-      $('#messages').append($('<li>').text('['+myName+']>> '+ msg));
+      $('#messages').prepend($('<li>').text('['+myName+']>> '+ msg));
       $('#input_msg').val('');
       return false;
     });
     socketio.on('message',function(msg){
       text = msg.replace('name',myName);
-      $('#messages').append($('<li>').text('[roa]>> '+text));
+      $('#messages').prepend($('<li>').text('[SEGA]>> '+text));
     });
 });
